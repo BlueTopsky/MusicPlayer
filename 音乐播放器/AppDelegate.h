8d2531b62d7e4129b78a2c3c9dcfbcbc74b3d81f
Topsky <Topsky@192.168.3.3>
2016-10-27 13:35:46 +0800

    Initial Commit

---
+++ 音乐播放器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  音乐播放器
+//
+//  Created by Topsky on 2016/10/27.
+//  Copyright © 2016年 Topsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
